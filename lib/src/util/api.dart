05f33163bdcb12becb7c44dc717ca620b1143ae3
Rizky Andhika Putra <rcdev.games@gmail.com>
2019-05-16 16:19:36 +0700

    env

--- lib/src/util/api.dart
+++ lib/src/util/api.dart
@@ -8,18 +8,18 @@ class Api {
 
   static Dio oAuth() {
     var dio = new Dio();
-    dio.options.baseUrl = Static.OAUTH_URL;
+    dio.options.baseUrl = Static.UAT_OAUTH_URL;
     dio.options.connectTimeout = 50000;
     dio.options.receiveTimeout = 15000;
     dio.options.contentType=ContentType.parse("application/x-www-form-urlencoded");
     // dio.options.headers = {"Authorization":"Basic ${Static.TOKENOAUTH}"};
-    dio.options.headers = {"Authorization":"Basic ${encode(Static.CLIENTID, Static.CLIENTSECRET)}"};
+    dio.options.headers = {"Authorization":"Basic ${encode(Static.UAT_CLIENTID, Static.UAT_CLIENTSECRET)}"};
     return dio;
   }
 
   static Dio access() {
     var dio = new Dio();
-    dio.options.baseUrl = Static.MCS_URL;
+    dio.options.baseUrl = Static.UAT_MCS_URL;
     dio.options.connectTimeout = 50000;
     dio.options.receiveTimeout = 15000;
     dio.options.headers = {"Content-Type": "application/json"};
